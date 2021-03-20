bddd0bc58cf350853bc37625e71be54ab87da6ba
marcelorpf <marcelorpf@gmail.com>
2021-03-20 10:57:42 -0300

    Finished?

--- datalab_hexsticker.R
+++ datalab_hexsticker.R
@@ -16,14 +16,16 @@ showtext_auto()
 
 
 p1 <- ggplot(aes(x = Petal.Length, y = Petal.Width, color = Species), data = iris) + geom_point()
-p1 <- p1 + theme_void() + theme_light() + theme(legend.position="none")
-p2 <- ggMarginal(p1, type="density")
+# p1 <- p1 + theme_void() + theme_bw() + theme(legend.position="none",
+#                                              panel.grid.major = element_blank(),
+#                                              panel.grid.minor = element_blank())
+p1 <- p1 + theme_void() + theme_transparent() + theme(legend.position="none")
+p2 <- ggMarginal(p1, type="histogram")
 p2
 
-s <- sticker(p2, package="DataLab",
-             s_x=1, s_y=1.6, s_width=.25,
-             p_size=60, p_y = 1, p_family = "gochi", p_color = "black",
-             h_color = "black", h_fill = "skyblue3", h_size = 1.2,
+s <- sticker(p2, s_x=1, s_y=1.2, s_width=.8,
+             package="DataLab", p_size=80, p_y = .6, p_family = "gochi", p_color = "black",
+             h_color = "black", h_fill = "skyblue4", h_size = 1.5,
              spotlight = TRUE, l_alpha = .5,
              url = "http://de.ufpb.br/datalab", u_y = 0.05, u_size = 12, u_family = "gochi",
              filename="/home/marcelo/Dropbox/DE-UFPB/DataLab/DataLab_hexsticker/logo.png")
